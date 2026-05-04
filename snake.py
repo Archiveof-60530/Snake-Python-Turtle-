d4b038a082e21b61b4fcda2ce520066229c819e1
Ryusuke Chapman <chappumanliu@gmail.com>
2026-05-04 16:31:07 -0500

    almost

--- snake.py
+++ snake.py
@@ -4,71 +4,98 @@ import random
 def generate_color():
     return f"#{random.randint(0, 0xFFFFFF):06x}"
 
-def playing_area():
-    pen = Turtle()
-    pen.ht()
-    pen.speed(0)
-    pen.color('light blue')
-    pen.begin_fill()
-    pen.goto(-240,240)
-    pen.goto(240,240)
-    pen.goto(240,-240)
-    pen.goto(-240,-240)
-    pen.goto(-240,240)
-    pen.end_fill()
-    
+
+
 class Head(Turtle):
   def __init__(self, screen, body):
     super().__init__()
-    pass
+    self.ht()
+    self.speed(0)
+    self.penup()
+    self.shape("square")
+    self.st()
+    screen.onkeypress(self.left, "Left")
+    screen.onkeypress(self.right, "Right")
+    screen.onkeypress(self.up, "Up")
+    screen.onkeypress(self.down, "Down")
 
   def up(self):
-    pass
+    if self.heading()!=-90:
+      self.setheading(90)
+    
 
   def down(self):
-    pass
+    if self.heading() != 90:
+      self.setheading(-90)
+    
+
 
   def left(self):
-    pass
+    if self.heading() != 0:
+      self.setheading(180)
+
 
   def right(self):
-    pass
+    if self.heading() != 180:
+     self.setheading(0)
+
 
   def move(self):
-    pass
-    
+    self.forward(5)
+    if self.xcor() > 230 or self.xcor() < -230:
+        self.die()
+    if self.ycor() > 230 or self.ycor() < -230:
+        self.die()
+
   def die(self):
-    pass
+    self.ht()
 
 
 class Segment(Turtle):
   def __init__(self, other):
     super().__init__()
-    pass
-
+    self.hideturtle()
+    self.penup()
+    self.speed(0)
+    self.shape("square")
+    self.showturtle()
   def move(self, other):
-    pass
+    for i in range(len(body)-1, 0, -1):
+      body[i].move(body[i-1])
+
 
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    pass
+    self.hideturtle()
+    self.penup()
+    self.speed(0)
+    self.shape("circle")
+    self.color("red")
+    self.goto(random.randint(-200,200), random.randint(-200,200))
+    self.showturtle()
 
   def relocate(self):
-    pass
+    self.goto(random.randint(-200,200), random.randint(-200,200))
 
 screen = Screen()
-screen.bgcolor("black")
+screen.bgcolor("lightblue")
 screen.setup(520,520)
 # Key Binding. Connects key presses and mouse clicks with function calls
 screen.listen()
 
+
 body = []
 
+player = Head(screen,body)
+apl = Apple()
 
-
-
-screen.exitonclick()
+while True:
+  player.move()
+  if player.distance(apl) < 20:
+    apl.relocate()
+    body.append(Segment(len(body)))
+    .move(body)     
 
 
 
